a493d6996de85c7f9332b15c0a73e2926826707d
Hsekharsahoo <himanshuss2001@gmail.com>
2022-03-14 15:48:04 +0530

    new

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Competitive Programmer", "Developer","Content Writer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
