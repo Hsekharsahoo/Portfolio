2d8a7733bfb456181786ac16c5e58b55923f8ed6
Hsekharsahoo <himanshuss2001@gmail.com>
2022-03-14 15:43:04 +0530

    typing

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Competitive Programmer", "Developer","Content Writer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
